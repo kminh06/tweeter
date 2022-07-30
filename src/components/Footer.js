494414259ccb359d089f5eed56862d28457e5f0a
KMinh <daukhacminh006@gmail.com>
2022-07-30 13:36:00 +0700

    Adding github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Github from '../media/github.png'
 
 export default function Footer(props) {
   return (
     <div id='footer' className={props.class}>
       <p>2022 &copy; Khac Minh Dau</p>
+      <a target='_blank' href='https://github.com/kminh06'><img src={Github} id='github' /></a>
     </div>
   )
 }
